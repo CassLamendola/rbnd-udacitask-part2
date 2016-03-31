d485be328bf25c4dd12ed9f61c2c7759cf8b45bd
Cassandra Lamendola <cass.lamendola@gmail.com>
2016-03-31 16:58:31 -0700

    Add method to format dates to listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,11 @@ module Listable
   def format_description(description)
   	description.ljust(30)
   end
+  def format_date(options = {})
+  	dates = options[:start].strftime("%D") if options[:start]
+  	dates << options[:end].strftime("%D") if options[:end]
+  	dates = options[:due_date].strftime("%D") if options[:due_date]
+  	dates = "N/A" if !dates
+  	return dates
+  end
 end
